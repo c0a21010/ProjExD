643b97d5f8e3ebfa4a6b6f41e51fe2fbbafe9aa7
C0A21010 <c0a21010be@edu.teu.ac.jp>
2022-06-14 15:43:05 +0900

    second_commit

---
+++ ex01/quiz.py
@@ -0,0 +1,41 @@
+import datetime
+import random
+
+class Quiz:
+    def __init__(self) -> None:
+        self.problems = {
+            "サザエの旦那の名前は?":["マスオ","ますお"],
+            "カツオの妹の名前は?":["ワカメ","わかめ"],
+            "タラオはカツオから見てどんな関係":["甥","おい","甥っ子","おいっこ"]
+        }
+
+        self.correct = "正解!"
+        self.incorrect = "出直してこい"
+
+    def startquiz(self):
+
+        problem_list = list(self.problems.keys())
+
+        random_problem = random.choice(problem_list)
+
+        print("問題:")
+        print(random_problem)
+
+        stime = datetime.datetime.now()
+        etime = datetime.datetime.now()
+
+        while True:
+
+            ans = input("答えるんだ:")
+            if ans in self.problems[random_problem]:
+                print(self.correct)
+                etime = datetime.datetime.now()
+                break
+            else:
+                print(self.incorrect)
+
+        print(etime-stime)
+        
+if __name__ == "__main__":
+    quiz = Quiz()
+    quiz.startquiz()
