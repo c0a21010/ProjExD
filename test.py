498ba1ee7debc7a0c88a15ef3fbdf2409bb27f5a
koya_aruga <c0a21010be@edu.teu.ac.jp>
2022-06-14 14:20:17 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+# print("Hello World")
+
+class Hello_world:
+    def __init__(self) -> None:
+        # pass
+        self.message = "Hello WOrld"
+    def hello_world(self):
+        print("Hello world")
+
+test = Hello_world()
+test.hello_world()
