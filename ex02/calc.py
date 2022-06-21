c7bf9bc54bdff227b8a40f908a42da1fd0d81ce2
koya_aruga <c0a21010be@edu.teu.ac.jp>
2022-06-21 14:39:53 +0900

    calcpy2

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,31 @@
 import tkinter as tk
+from tkinter import font
 
 root = tk.Tk()
 root.geometry("300x500")
+
+buttonlist = []
+for i in range(3):
+    for j in range(3):
+        buttonlist.append(
+            tk.Button(
+                root
+                ,text=f"{9-(i*3+j)}"
+                ,width=4
+                ,height=2
+                ,font=("Times New Roman",30))
+        )
+        buttonlist[len(buttonlist)-1].grid(row=i,column=j)
+
+buttonlist.append(
+            tk.Button(
+                root
+                ,text=f"{10}"
+                ,width=4
+                ,height=2
+                ,font=("Times New Roman",30))
+        )
+buttonlist[len(buttonlist)-1].grid(row=3,column=0)
+
 
 root.mainloop()
