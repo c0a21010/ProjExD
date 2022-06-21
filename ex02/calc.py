53bc18d0e5a1a29482bed63cd8b23d4ef7b836f9
koya_aruga <c0a21010be@edu.teu.ac.jp>
2022-06-21 15:07:44 +0900

    calcpy4

--- ex02/calc.py
+++ ex02/calc.py
@@ -8,32 +8,24 @@ def printwindow():
 root = tk.Tk()
 root.geometry("300x500")
 
+entry = tk.Entry(justify="right",width=10,font=("Times New Roman",40))
+entry.insert(tk.END,"fugapiyo")
+entry.grid(row=0,column=0,columnspan = 3)
+
+numlist = [i for i in range(9,-1,-1)]
+
 buttonlist = []
-for i in range(3):
-    for j in range(3):
-        buttonlist.append(
-            tk.Button(
-                root
-                ,text=f"{9-(i*3+j)}"
-                ,width=4
-                ,height=2
-                ,font=("Times New Roman",30)
-                ,command=printwindow
-                )
-        )
-        buttonlist[len(buttonlist)-1].grid(row=i,column=j)
-
-buttonlist.append(
-            tk.Button(
-                root
-                ,text=f"{10}"
-                ,width=4
-                ,height=2
-                ,font=("Times New Roman",30)
-                ,command=printwindow
-                )
-        )
-buttonlist[len(buttonlist)-1].grid(row=3,column=0)
-
+for i in range(10):
+    buttonlist.append(
+        tk.Button(
+            root
+            ,text=f"{numlist[i]}"
+            ,width=4
+            ,height=2
+            ,font=("Times New Roman",30)
+            ,command=printwindow
+            )
+    )
+    buttonlist[len(buttonlist)-1].grid(row=(i//3)+1,column=i%3)
 
 root.mainloop()
