b7bcf70de479b215afd6f0f970ec6cb647e44720
koya_aruga <c0a21010be@edu.teu.ac.jp>
2022-06-21 14:42:53 +0900

    calcpy3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import font
+import tkinter.messagebox as tkm
+
+def printwindow():
+    tkm.showwarning("おしたな","おしたな")
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -13,7 +17,9 @@ for i in range(3):
                 ,text=f"{9-(i*3+j)}"
                 ,width=4
                 ,height=2
-                ,font=("Times New Roman",30))
+                ,font=("Times New Roman",30)
+                ,command=printwindow
+                )
         )
         buttonlist[len(buttonlist)-1].grid(row=i,column=j)
 
@@ -23,7 +29,9 @@ buttonlist.append(
                 ,text=f"{10}"
                 ,width=4
                 ,height=2
-                ,font=("Times New Roman",30))
+                ,font=("Times New Roman",30)
+                ,command=printwindow
+                )
         )
 buttonlist[len(buttonlist)-1].grid(row=3,column=0)
 
