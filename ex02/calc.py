64e5f392b21b107b5f3402b6f33b542ecf0be2f6
koya_aruga <c0a21010be@edu.teu.ac.jp>
2022-06-21 15:11:27 +0900

    calcpy5

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,12 +4,15 @@ import tkinter.messagebox as tkm
 
 def printwindow():
     tkm.showwarning("おしたな","おしたな")
+def clickbutton(event):
+    btn = event.widget
+    txt = btn["text"]
+    entry.insert(tk.END,txt)
 
 root = tk.Tk()
 root.geometry("300x500")
 
 entry = tk.Entry(justify="right",width=10,font=("Times New Roman",40))
-entry.insert(tk.END,"fugapiyo")
 entry.grid(row=0,column=0,columnspan = 3)
 
 numlist = [i for i in range(9,-1,-1)]
@@ -23,9 +26,9 @@ for i in range(10):
             ,width=4
             ,height=2
             ,font=("Times New Roman",30)
-            ,command=printwindow
             )
     )
+    buttonlist[len(buttonlist)-1].bind("<1>",clickbutton)
     buttonlist[len(buttonlist)-1].grid(row=(i//3)+1,column=i%3)
 
 root.mainloop()
