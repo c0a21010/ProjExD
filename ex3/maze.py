13a52e29b6c79beabc4ff07806a8b964cd84b7a7
koya_aruga <c0a21010be@edu.teu.ac.jp>
2022-06-28 15:26:15 +0900

    3-7

--- ex3/maze.py
+++ ex3/maze.py
@@ -2,6 +2,12 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 from matplotlib import image, widgets
+from numpy import imag
+
+global key, cx, cy
+key = ""
+cx = 300
+cy = 400
 
 class Application(tk.Frame):
     def __init__(self,master) -> None:
@@ -10,8 +16,6 @@ class Application(tk.Frame):
         self.master.geometry()
 
         # self.key = ""
-        global key
-        key = ""
 
 
         self.canvas = tk.Canvas(
@@ -23,23 +27,54 @@ class Application(tk.Frame):
 
         self.kokaton = tk.PhotoImage(file="5.png")
 
-        global cx, cy
-        cx = 300
-        cy = 400
-
         self.canvas.create_image(cx,cy,image=self.kokaton,tag="kokaton")
 
         self.canvas.pack()
 
         self.master.bind("<KeyPress>", self.key_down)
-        self.master.bind("<KeyRelease>", self.key_down)
+        self.master.bind("<KeyRelease>", self.key_up)
+
+        self.main_proc()
 
     def key_down(self,event):
+        global key
         key = event.keysym
+        # print(key)
 
     def key_up(self,event):
         global key
         key = ""
+
+    def main_proc(self):
+        move = {
+            "Up":-20j,
+            "Down":20j,
+            "Left":-20,
+            "Right":20,
+        }
+
+        global cx, cy
+
+        cx_cy = complex(cx,cy)
+
+        if key in move.keys():
+
+            # print(key)
+
+            cx_cy += move[key]
+
+            # print(move[key])
+
+            # print(cx_cy)
+
+            cx = cx_cy.real
+            cy = cx_cy.imag
+
+            self.canvas.coords("kokaton",cx,cy)
+
+        
+
+        self.master.after(100,self.main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
