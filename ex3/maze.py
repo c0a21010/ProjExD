02245e6ac52cec9319f92706d25f3e81cfa33704
koya_aruga <c0a21010be@edu.teu.ac.jp>
2022-06-28 14:30:05 +0900

    3-2

--- ex3/maze.py
+++ ex3/maze.py
@@ -1,9 +1,23 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+from matplotlib import widgets
+
 class Application(tk.Frame):
     def __init__(self,master) -> None:
         super().__init__(master)
+
+        self.master.geometry()
+
+
+        canvas = tk.Canvas(
+            self.master,
+            width = 1500,
+            height = 900,
+            bg="black"
+        )
+
+        canvas.pack()
 
 if __name__ == "__main__":
     root = tk.Tk()
