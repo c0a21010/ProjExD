7e346197877f4cd294a91f47e660f938cccece23
absent2223 <kouya555000888@gmail.com>
2022-07-05 15:06:50 +0900

    p1-2

---
+++ ex04/dodge_bomb.py
@@ -0,0 +1,26 @@
+import pygame as pg
+import sys
+
+
+def main():
+
+    clock = pg.time.Clock()
+
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen = pg.display.set_mode((1600, 900))
+    bg_img = pg.image.load(
+        "/home/koya/MEGA/講義資料/2_前期/プロジェクト演習/3クール/ProjExD/ex04/pg_bg.jpg"
+    )
+    bg_rect = bg_img.get_rect()
+    screen.blit(bg_img, bg_rect)
+
+    pg.display.update()
+
+    clock.tick(0.5)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
